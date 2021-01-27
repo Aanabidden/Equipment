e7835f5c8f3bd3f82def641f4a6c008b1ddfc162
Aradhana <aradhanaverma@eternalrobotics.com>
2021-01-27 10:11:07 +0530

    Initial Commit

---
+++ Equipment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Equipment
+//
+//  Created by Aradhana on 27/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
